25b522c533878e7b325984e3a189db426203926a
stevenkilty03 <stevenkilty2@gmail.com>
2024-10-31 16:25:17 +0000

    Revert "Header Updated"
    This reverts commit 4b669aac0100c40aecaffdaf055bb8aad22cc694.

--- KV6002/src/components/Header.jsx
+++ KV6002/src/components/Header.jsx
@@ -1,178 +1,48 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  Typography,
-  InputBase,
-  IconButton,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import LanguageIcon from "@mui/icons-material/Language";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import charityLogo from "../assets/Rose_logo.png";
-import "../App.css";
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import charityLogo from "../assets/logo.jpg"; // Update the path as necessary
+import "../App.css"; // Importing App.css from the src directory
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
     <Box>
       {/* Top Part of the Header */}
       <Box
         sx={{
-          backgroundColor: "#7B3F3F",
+          backgroundColor: "#7B3F3F", // Darker shade
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
           padding: "1rem",
         }}
       >
-        {/* Burger Icon for Menu */}
-        <IconButton
-          onClick={toggleMenu}
-          sx={{ color: "white" }}
-        >
-          <MenuIcon />
-        </IconButton>
-
-        <Typography
-          variant="h6"
-          sx={{ color: "white", textAlign: "center", flexGrow: 1 }}
-        >
+        <Typography variant="h6" sx={{ color: 'white', textAlign: 'center', flexGrow: 1 }}>
           Together we can make a difference
         </Typography>
+        <Button variant="contained" sx={{ backgroundColor: 'green', color: 'white' }}>
+          Language
+        </Button>
       </Box>
-
-      {/* Drawer for Menu Links */}
-      <Drawer
-        anchor="left"
-        open={menuOpen}
-        onClose={toggleMenu}
-      >
-        <Box
-          sx={{ width: 250, padding: "1rem" }}
-          onClick={toggleMenu}
-        >
-          <List>
-            <ListItem
-              button
-              component={Link}
-              to="/"
-            >
-              <ListItemText primary="Home" />
-            </ListItem>
-            <ListItem
-              button
-              component={Link}
-              to="/events"
-            >
-              <ListItemText primary="Events" />
-            </ListItem>
-            <ListItem
-              button
-              component={Link}
-              to="/about"
-            >
-              <ListItemText primary="About Us" />
-            </ListItem>
-            <ListItem
-              button
-              component={Link}
-              to="/help"
-            >
-              <ListItemText primary="Help Centre" />
-            </ListItem>
-            <ListItem
-              button
-              component={Link}
-              to="/events-list-admin-view"
-            >
-              <ListItemText primary="Events Admin View" />
-            </ListItem>
-            <ListItem
-              button
-              component={Link}
-              to="/login"
-            >
-              <ListItemText primary="Login" />
-            </ListItem>
-          </List>
-        </Box>
-      </Drawer>
 
       {/* Bottom Part of the Header */}
       <Box
         sx={{
-          backgroundColor: "#D08C8C",
+          backgroundColor: "#D08C8C", // Lighter shade
           display: "flex",
           alignItems: "center",
           padding: "1rem",
         }}
       >
-        {/* Language Button */}
-        <IconButton sx={{ color: "black" }}>
-          <LanguageIcon />
-        </IconButton>
-
-        {/* Logo */}
-        <img
-          src={charityLogo}
-          alt="Charity Logo"
-          style={{ width: "75px", marginLeft: "1rem", marginRight: "auto" }}
-        />
-
-        {/* Search Bar */}
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            flexGrow: 1,
-            maxWidth: "400px",
-            margin: "0 2rem",
-          }}
-        >
-          <InputBase
-            placeholder="Search by event name"
-            sx={{
-              backgroundColor: "white",
-              padding: "0.5rem 1rem",
-              borderRadius: "4px",
-              width: "100%",
-            }}
-          />
-          <IconButton
-            type="submit"
-            aria-label="search"
-            sx={{ color: "black" }}
-          >
-            <SearchIcon />
-          </IconButton>
-        </Box>
-
-        {/* Login Button */}
-        <Button
-          component={Link}
-          to="/login"
-          startIcon={<AccountCircleIcon />}
-          sx={{
-            color: "black",
-            textTransform: "none",
-            fontWeight: "bold",
-            marginLeft: "auto",
-          }}
-        >
-          Login
-        </Button>
+        <img src={charityLogo} alt="Charity Logo" style={{ width: '150px', marginRight: 'auto' }} />
+        <nav style={{ display: 'flex', justifyContent: 'center', flexGrow: 1 }}>
+          <Link to="/" style={{ margin: '0 1rem', color: 'black', fontWeight: 'bold', textDecoration: 'none' }}>Home</Link>
+          <Link to="/events" style={{ margin: '0 1rem', color: 'black', fontWeight: 'bold', textDecoration: 'none' }}>Events</Link>
+          <Link to="/about" style={{ margin: '0 1rem', color: 'black', fontWeight: 'bold', textDecoration: 'none' }}>About Us</Link>
+          <Link to="/help" style={{ margin: '0 1rem', color: 'black', fontWeight: 'bold', textDecoration: 'none' }}>Help Centre</Link>
+          <Link to="/events-list-admin-view" style={{ margin: '0 1rem', color: 'black', fontWeight: 'bold', textDecoration: 'none' }}>Events Admin View </Link> {/* Admin Events link */}
+          <Link to="/login" style={{ margin: '0 1rem', color: 'black', fontWeight: 'bold', textDecoration: 'none' }}>Login</Link>
+        </nav>
       </Box>
     </Box>
   );
